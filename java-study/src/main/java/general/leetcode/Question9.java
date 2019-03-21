47457046548b2798a86266373b70a31eaf92e0f0
javon <javon_wang@163.com>
2019-03-21 17:22:52 +0800

    9:回文数，总算有一个可以5分钟写出来的题了

--- java-study/src/main/java/general/leetcode/Question9.java
+++ java-study/src/main/java/general/leetcode/Question9.java
@@ -1,5 +1,20 @@
 package general.leetcode;
 
+/**
+ * 9. 回文数	
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * @author javon
+ *
+ */
 public class Question9 {
-
+	
+ 	public boolean isPalindrome(int x) {
+ 		String string = String.valueOf(x);
+ 		for (int i = 0; i < string.length()/2; i++) {
+			if(string.charAt(i) != string.charAt(string.length() - 1 -i))
+				return false;
+		}
+ 		return true;
+    }
+	
 }
